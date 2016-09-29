6c81fdcc2c4f6cc3cd7aba0c55971945368f2c23
Takaki <p104314t@mail.kyutech.jp>
2016-09-29 15:56:55 +0200

    modified

--- ros_ws/build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/student/Documents/AL5D_in_Salento/ros_ws/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
