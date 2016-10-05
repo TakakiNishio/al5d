997ceb5ba2e6a00b4af6464dfc3d8944eb5efb97
Takaki <p104314t@mail.kyutech.jp>
2016-10-05 17:19:27 +0200

    simulation model

--- src/python/move_in_rviz.py
+++ src/python/move_in_rviz.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python
 
+import math as m
+
 import rospy
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Header
+
+def deg2rad(deg):
+
+    rad = []
+    rad.append(float(deg[0]*(m.pi/180)))
+    rad.append(float(deg[1]*(m.pi/180)))
+    rad.append(float(deg[2]*(m.pi/180)))
+    rad.append(float(deg[3]*(m.pi/180)))
+
+    return rad
+
 
 def talker(theta):
     pub = rospy.Publisher('joint_states', JointState, queue_size=10)
@@ -17,9 +30,24 @@ def talker(theta):
     al5d_str.effort = []
 
     while not rospy.is_shutdown():
-        hello_str.header.stamp = rospy.Time.now()
-        pub.publish(hello_str)
+        al5d_str.header.stamp = rospy.Time.now()
+        pub.publish(al5d_str)
         rate.sleep()
 
 if __name__ == '__main__':
-    talker()
+
+    theta_deg = []
+
+    print "imput each joint angle in deg."
+    print "theta_base:"
+    theta_deg.append(float(raw_input('>>>  ')))
+    print "theta_shoulder:"
+    theta_deg.append(float(raw_input('>>>  ')))
+    print "theta_elbow:"
+    theta_deg.append(float(raw_input('>>>  ')))
+    print "theta_wrist:"
+    theta_deg.append(float(raw_input('>>>  ')))
+
+    theta_rad = deg2rad(theta_deg)
+
+    talker(theta_rad)
